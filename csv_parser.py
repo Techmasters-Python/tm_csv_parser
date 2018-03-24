382b68e42a7f79e6373011a64866b340f76f7262
Aniruddha Adhikary <aniruddha@adhikary.net>
2018-03-24 17:11:16 +0600

    shortening things up

--- csv_parser.py
+++ csv_parser.py
@@ -1,26 +1,8 @@
 import csv
 
 
-def get_csv_data(file_path='students.csv'):
-    """
-    Get the list of students along with organization.
-    """
-    with open(file_path) as csv_file:
-        # this with thingy is called a "context manager"
-        # google it up ;)
-        return csv_file.read()
-
-
-def get_lines(csv_data):
-    """
-    Split individual lines from a csv text
-    """
-    return csv_data.splitlines()
-
-
-def get_people(csv_data):
-    lines = get_lines(csv_data)
-    return csv.DictReader(lines)
+def get_people(csv_file):
+    return csv.DictReader(csv_file)
 
 
 def print_people(people):
@@ -29,9 +11,9 @@ def print_people(people):
 
 
 def main():
-    csv_data = get_csv_data()
-    people = get_people(csv_data)
-    print_people(people)
+    with open('students.csv') as csv_file:
+        people = get_people(csv_file)
+        print_people(people)
 
 
 if __name__ == '__main__':
--- test_csv_parser.py
+++ test_csv_parser.py
@@ -1,15 +1,15 @@
-from csv_parser import get_lines, get_people
+from io import StringIO
 
-
-def test_get_lines():
-    assert get_lines('a\nb') == ['a', 'b']
+from csv_parser import get_people
 
 
 def test_get_people():
-    data = "name,org,is_student\na,b,c\nc,d,e\n"
-    output = list(get_people(data))
-    expectation = [
-        {'name': 'a', 'org': 'b', 'is_student': 'c'},
-        {'name': 'c', 'org': 'd', 'is_student': 'e'},
-    ]
-    assert output == expectation
+    # What is StringIO? Google!
+    with StringIO("name,org,is_student\na,b,c\nc,d,e\n") as data:
+        # simulates a file object ;) we are mocking!
+        output = list(get_people(data))
+        expectation = [
+            {'name': 'a', 'org': 'b', 'is_student': 'c'},
+            {'name': 'c', 'org': 'd', 'is_student': 'e'},
+        ]
+        assert output == expectation
